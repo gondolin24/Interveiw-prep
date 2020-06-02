2099e1d8537bf6d245449a244948a513db992439
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-06-02 17:33:28 -0400

    dont foreget num vertex

--- amazon/min-cost-to-repair-edges.js
+++ amazon/min-cost-to-repair-edges.js
@@ -26,14 +26,15 @@ function remove(i,j,matrix){
 }
 
 
-function cycleFormed(i,j,matrix){
+function vertexExits(i,j,matrix){
     const der = matrix[i-1][j-1]!==Infinity
     return der
 }
 
-function repair(edgesToRepair,matrix) {
+function repair(edgesToRepair,matrix,numvertex,max) {
     edgesToRepair.forEach((a)=>{
         const [val,value] = a
+        numvertex = numvertex-1
         remove(val-1,value-1,matrix)
     })
 
@@ -44,29 +45,32 @@ function repair(edgesToRepair,matrix) {
     while(sortedEdges.length>1){
         const [i,j,repairCost] = sortedEdges.shift()
 
-        if(!cycleFormed(i,j,matrix)){
+        if(!vertexExits(i,j,matrix)&& numvertex< max-1){
             cost = cost+repairCost
             add(i-1,j-1,matrix, repairCost)
+            numvertex = numvertex+1
         }
-
     }
 
     return cost
 }
 function solve(numNodes, edges, edgedToRepair) {
     const matrix = generateMatrix(numNodes)
+    let numvertex = 0
     edges.forEach((edge)=>{
         const[a,b] = edge
         add(a-1,b-1,matrix,0)
+        numvertex = numvertex+1
     })
-    return repair(edgedToRepair,matrix)
+
+    return repair(edgedToRepair,matrix,numvertex, numNodes)
 }
 let n = 5
 let edgez  = [[1, 2], [2, 3], [3, 4], [4, 5], [1, 5]]
 let repairMe= [[1, 2, 12], [3, 4, 30], [1, 5, 8]]
 
 console.log(solve(n,edgez,repairMe)===20)
-
+//
 n = 6
 edgez = [[1, 2], [2, 3], [4, 5], [3, 5], [1, 6], [2, 4]]
 repairMe = [[1, 6, 410], [2, 4, 800]]
@@ -75,4 +79,4 @@ console.log(solve(n,edgez,repairMe)===410)
 n = 6
 edgez = [[1, 2], [2, 3], [4, 5], [5, 6], [1, 5], [2, 4], [3, 4]]
 repairMe = [[1, 5, 110], [2, 4, 84], [3, 4, 79]]
-console.log(solve(n,edgez,repairMe))
+console.log(solve(n,edgez,repairMe)===79)
