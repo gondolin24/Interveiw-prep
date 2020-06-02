988d7360d2e4ae729f61c8daac81bbc3f392660b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-06-01 20:56:00 -0400

    there was a better way :)

--- amazon/longest-string-vowel.js
+++ amazon/longest-string-vowel.js
@@ -2,90 +2,56 @@ function isVowel(char) {
     return 'aeiou'.includes(char)
 }
 
-function isLastVowel(array) {
-    if (array.length === 0) return false
-    return isVowel(array[array.length - 1])
-
+function isPreviousVowel(index, arr) {
+    if (index < 0) {
+        return isVowel(arr[arr.length - 1])
+    } else {
+        return isVowel(arr[index])
+    }
 }
 
-function hasVowelsLeft(list) {
-    if (list.length === 0) return false
-    return list.filter((char) => isVowel(char)).length > 0
-}
-
-function getNextNode(array) {
-    let current = null
-    let count = 0
-    while (array.length > 0 && current === null) {
-        const char = array.shift()
-        if (isVowel(char)) {
-            current = {
-                cost: count > 0 ? 1 : 0
-            }
+function getList(array) {
+    let circularLinkedList = []
+    for (let i = 0; i < array.length; i++) {
+        const current = array[i]
+        if (isVowel(current)) {
+            const cost = isPreviousVowel(i-1, array) ? 0 : 1
+            circularLinkedList.push(cost)
         }
-        count = count + 1
     }
-    return current
+    return circularLinkedList
 }
 
-
-function insert(node, linkedList) {
-    let current = linkedList
-
-    while (current.next != null) {
-        current = current.next
-    }
-    current.next = node
-}
-
-function derp(node, max) {
-    let cost = node.cost
+function findMax(index, array) {
+    const arrayLen = array.length
+    let cost = array[index]
     let count = 1
-    let current = node
-    while (cost < 3 && count < max) {
-        const next = current.next
-        cost = cost + next.cost
+    let i = index
+    while (count < arrayLen && cost < 3) {
+        const next = (i + 1) % arrayLen
+        cost = cost + array[next]
         if (cost < 3) {
             count = count + 1
         }
-        current = next
+        i = next
     }
     return count
 }
 
-function solve(string) {
-    const arr = string.split('')
-    const nodeList = []
-    const finalSum = isLastVowel(arr) ? 0 : 1
-
-    while (hasVowelsLeft(arr)) {
-        const current = getNextNode(arr)
-        nodeList.push(current)
-    }
-
-    const max = nodeList.length
-    if (max === 0) return 0
-
-    let linkedList = nodeList.shift()
-    linkedList.cost = linkedList.cost+ finalSum
-    while (nodeList.length > 0) {
-        const current = nodeList.shift()
-        if (nodeList.length === 0) {
-            current.next = linkedList
+function solve(str) {
+    const strArray = str.split('')
+    const circularLinkedList = getList(strArray)
+    let max = 0
+    for (let i = 0; i < circularLinkedList.length; i++) {
+        const current = findMax(i, circularLinkedList)
+        if (current > max) {
+            max = current
         }
-        insert(current, linkedList)
     }
-
-    const sumArr = []
-    let current = linkedList
-    for (let i = 0; i < max; i++) {
-        sumArr.push(derp(current, max))
-        current = current.next
-    }
-
-    return sumArr.sort((a, b) => b - a).shift()
+    return max
 }
-console.log(solve('earthproblem')===3)
 
-console.log(solve('letsgosomewhere')===2)
+console.log(solve('earthproblem') === 3)
+
+console.log(solve('letsgosomewhere') === 2)
 //testCases
